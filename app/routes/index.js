604aba7d969f9ff4ef72ca1c5a3253bc9f73ff1c
Hareesh Nagaraj <hareesh.nagaraj@gmail.com>
2015-01-07 22:54:30 -0500

    functioning login w/graph access

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var FB = require('fb');
 var router = express.Router();
+var userAccessCode;
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -7,8 +9,42 @@ router.get('/', function(req, res) {
 });
 
 router.get('/active', function(req, res) {
-  res.render('active', { title: 'Social Analytics' });
+    userAccessCode = req.param("code");  //this is the user's access code, used to get token
+    console.log('\n');
+
+    FB.api('oauth/access_token', {
+        client_id: '-',
+        client_secret: '-',
+        redirect_uri: 'http://localhost:3000/active/',
+        code: userAccessCode
+    }, function (res) {
+        if(!res || res.error) {
+            console.log(!res ? 'error occurred' : res.error);
+            return;
+        }
+
+        var accessToken = res.access_token;
+
+        var expires = res.expires ? res.expires : 0;
+        console.log('\n');
+        console.log('\n');
+        console.log(accessToken)
+        console.log(expires)
+    });
+
+  res.render('active', { response : res });
 });
+
+
+// FB.api('me', { access_token : userAccessToken } ,function (res) {
+//     if(!res || res.error) {
+//      console.log(!res ? 'error occurred' : res.error);
+//      return;
+//     }
+//     console.log(res.id);
+//     console.log(res.name);
+// });
+
 
 
 
